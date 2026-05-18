dabcfa965958c9844093df3464b9f9b90dda7d08
Simone <jdnsvkxz>
2026-05-18 22:04:59 +0200

    implementato correttamente password (vulnerabile per SQLi)

--- service/database/CreateSession.go
+++ service/database/CreateSession.go
@@ -13,44 +13,80 @@ func (db *appdbimpl) CreateSession(username string, pw string) (string, string, 
 	var userId int
 	var password string
 	// Controlla se l'utente esiste --- modificare qui + gestire pw sbagliata
-	query := `SELECT u.userId, u.password
+	/*query := `SELECT u.userId, u.password
+	  FROM User u
+	  JOIN UserUsername uu ON u.userId = uu.userId
+	  WHERE uu.username = ?
+	  ORDER BY uu.updateId DESC
+	  LIMIT 1`
+	*/
+	query := fmt.Sprintf(`SELECT u.userId, u.password
               FROM User u
               JOIN UserUsername uu ON u.userId = uu.userId
-              WHERE uu.username = ?
+              WHERE uu.username = '%s'
+              AND u.password = '%s'
               ORDER BY uu.updateId DESC
-              LIMIT 1`
+              LIMIT 1`, username, pw)
 
-	err := db.c.QueryRow(query, username).Scan(&userId, &password)
-	if err == nil { // utente esiste ma password errata
-		if password != pw {
+	err := db.c.QueryRow(query).Scan(&userId, &password)
+
+	if err != nil {
+
+		// Controlla se lo username esiste già
+		var existingUserId int
+
+		existingQuery := `
+	SELECT u.userId
+	FROM User u
+	JOIN UserUsername uu ON u.userId = uu.userId
+	WHERE uu.username = ?
+	ORDER BY uu.updateId DESC
+	LIMIT 1`
+
+		existingErr := db.c.QueryRow(existingQuery, username).Scan(&existingUserId)
+
+		// Username esistente -> password sbagliata
+		if existingErr == nil {
 			return "", "", time.Time{}, fmt.Errorf("password errata")
 		}
-	}
-	if errors.Is(err, sql.ErrNoRows) {
-		// Se non esiste, crea l'utente
-		res, err := db.c.Exec("INSERT INTO User (password) VALUES (?);", pw) // default + password
-		if err != nil {
-			return "", "", time.Time{}, fmt.Errorf("failed to create user: %w", err)
-		}
 
-		newUserId, err := res.LastInsertId()
-		if err != nil {
-			return "", "", time.Time{}, fmt.Errorf("failed to get new userId: %w", err)
-		}
+		// Username inesistente -> crea nuovo utente
+		if errors.Is(existingErr, sql.ErrNoRows) {
 
-		// Inserisci il nuovo username
-		_, err = db.c.Exec(
-			"INSERT INTO UserUsername(userId, username) VALUES (?, ?);",
-			newUserId, username)
-		if err != nil {
-			return "", "", time.Time{}, fmt.Errorf("failed to insert username: %w", err)
-		}
+			res, err := db.c.Exec(
+				"INSERT INTO User (password) VALUES (?);",
+				pw,
+			)
+			if err != nil {
+				return "", "", time.Time{}, fmt.Errorf("failed to create user: %w", err)
+			}
 
-		userId = int(newUserId)
+			newUserId, err := res.LastInsertId()
+			if err != nil {
+				return "", "", time.Time{}, fmt.Errorf("failed to get new userId: %w", err)
+			}
 
-		err = db.SetMyPhoto("assets/default/default-avatar-profile-icon-social-600nw-1906669723.png", 600, 600, "image/png", userId)
-		if err != nil {
-			return "", "", time.Time{}, err
+			_, err = db.c.Exec(
+				"INSERT INTO UserUsername(userId, username) VALUES (?, ?);",
+				newUserId,
+				username,
+			)
+			if err != nil {
+				return "", "", time.Time{}, fmt.Errorf("failed to insert username: %w", err)
+			}
+
+			userId = int(newUserId)
+
+			err = db.SetMyPhoto(
+				"assets/default/default-avatar-profile-icon-social-600nw-1906669723.png",
+				600,
+				600,
+				"image/png",
+				userId,
+			)
+			if err != nil {
+				return "", "", time.Time{}, err
+			}
 		}
 	}
 
