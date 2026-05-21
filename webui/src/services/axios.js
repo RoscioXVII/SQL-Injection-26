fcffa2a6b4ba24d56d1398d7fe623606a6a93262
RoscioXVII <125834287+RoscioXVII@users.noreply.github.com>
2026-05-21 15:55:14 +0200

    Aggiunta XSS

--- webui/src/services/axios.js
+++ webui/src/services/axios.js
@@ -16,7 +16,7 @@ instance.interceptors.request.use(config => {
 
 	return config;
 });
-// modificare questa cacata
+
 export const doLogin = async (username, password,isLogin) => {
 	try {
 		const response = await instance.post(`/session?isLogin=${isLogin}`, {
